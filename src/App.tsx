718acb3dc7a02c7ff1b1f1acba369794ba6c0b70
unknown <anhlct@vmodev.com>
2022-03-04 16:26:41 +0700

    file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
-import "./App.scss";
+import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Home />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeProvider } from "styled-components";
 
 import App from "./App";
+import { GlobalStyle, theme } from "./styles";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
