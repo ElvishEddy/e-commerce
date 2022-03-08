00b9bfaac13a994640684e5e41eb67655e5681e0
unknown <anhlct@vmodev.com>
2022-03-08 15:33:45 +0700

    Complete Product Detail

--- src/pages/product-detail/ProductDetail.tsx
+++ src/pages/product-detail/ProductDetail.tsx
@@ -1,16 +1,35 @@
 import React from "react";
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 
 import { Announcement, Navbar, Newsletter, Footer } from "../../components";
+import {
+  Wrapper,
+  ImageProduct,
+  Info,
+  Price,
+  FilterGroup,
+  Filter,
+  AddContainer,
+  AmountContainer,
+  Amount,
+  FilterTitle,
+  ColorOption,
+  Button,
+} from "./styles";
+
 export const ProductDetail = () => {
   return (
     <>
       <Navbar />
       <Announcement />
-      <div>
-        <div>
-          <img src="" alt="" />
-        </div>
-        <div>
+      <Wrapper>
+        <ImageProduct>
+          <img
+            src="https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png"
+            alt="shirt"
+          />
+        </ImageProduct>
+        <Info>
           <h2>Denim Jumpsuit</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
@@ -18,32 +37,44 @@ export const ProductDetail = () => {
             minus deleniti illo, in reiciendis dolor, accusantium sed facere
             similique, libero molestias temporibus.
           </p>
-          <span>$ 20</span>
-          <div>
-            <span>Color:</span>
-            <select>
-              <option value="white">White</option>
-              <option value="black">Black</option>
-              <option value="red">Red</option>
-            </select>
-          </div>
+          <Price>$ 20</Price>
 
-          <div>
-            <span>Size:</span>
-            <select>
-              <option disabled selected>
-                Size
-              </option>
-              <option value="xs">XS</option>
-              <option value="s">S</option>
-              <option value="m">M</option>
-              <option value="l">L</option>
-              <option value="xl">XL</option>
-              <option value="xxl">XXL</option>
-            </select>
-          </div>
-        </div>
-      </div>
+          <FilterGroup>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <ColorOption color="black" />
+              <ColorOption color="darkblue" />
+              <ColorOption color="gray" />
+            </Filter>
+
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <select>
+                <option disabled selected>
+                  Size
+                </option>
+                <option value="xs">XS</option>
+                <option value="s">S</option>
+                <option value="m">M</option>
+                <option value="l">L</option>
+                <option value="xl">XL</option>
+                <option value="xxl">XXL</option>
+              </select>
+            </Filter>
+          </FilterGroup>
+
+          <AddContainer>
+            <AmountContainer>
+              <MinusOutlined style={{ fontSize: "20px", cursor: "pointer" }} />
+              <Amount>1</Amount>
+              <PlusOutlined style={{ fontSize: "20px", cursor: "pointer" }} />
+            </AmountContainer>
+
+            <Button>Add to cart</Button>
+          </AddContainer>
+        </Info>
+      </Wrapper>
+
       <Newsletter />
       <Footer />
     </>
